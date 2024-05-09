1c5ade30d2ca0eed92a6697b05a8a9089d35678c
Caio Cezar Dias <caiocd007@gmail.com>
2024-05-08 23:14:05 -0300

    Bug fix

--- src/main/java/com/uemg/biblioteca_universitaria/view/boxString.java
+++ src/main/java/com/uemg/biblioteca_universitaria/view/boxString.java
@@ -6,10 +6,8 @@ package com.uemg.biblioteca_universitaria.view;
 
 public class boxString {
 public static void boxString(String conteudoString) {
-    // Divide a string original em linhas
     String[] linhas = conteudoString.split("\n");
     
-    // Determina o comprimento máximo de linha
     int maxLength = 0;
     for (String linha : linhas) {
         if (linha.length() > maxLength) {
@@ -17,23 +15,19 @@ public static void boxString(String conteudoString) {
         }
     }
     
-    // Imprime a linha superior da caixa
     for (int i = 0; i < maxLength + 2; i++) {
         System.out.print("-");
     }
     System.out.println();
     
-    // Imprime cada linha dentro de uma borda
     for (String linha : linhas) {
         System.out.print("|" + linha);
-        // Preenche o espaço restante na linha com espaços, se necessário
         for (int i = linha.length(); i < maxLength; i++) {
             System.out.print(" ");
         }
         System.out.println("|");
     }
     
-    // Imprime a linha inferior da caixa
     for (int i = 0; i < maxLength + 2; i++) {
         System.out.print("-");
     }
