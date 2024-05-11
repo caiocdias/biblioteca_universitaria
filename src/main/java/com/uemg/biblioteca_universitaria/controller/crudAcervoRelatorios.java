1b86d42629f6cd21b18dc75663baf7e72be381a1
Caio Cezar Dias <caiocd007@gmail.com>
2024-05-10 23:32:33 -0300

    Adicionado crudAcervoRelatorios

---
+++ src/main/java/com/uemg/biblioteca_universitaria/controller/crudAcervoRelatorios.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uemg.biblioteca_universitaria.controller;
+import static com.uemg.biblioteca_universitaria.controller.crudAcervo.*;
+import com.uemg.biblioteca_universitaria.model.classes.acervoRelatorios;
+import java.util.Scanner;
+
+public class crudAcervoRelatorios {
+    public static boolean cadastrarAcervoRelatorios(acervoRelatorios acervoRelatorios) {
+        if (cadastrarAcervo(acervoRelatorios)) {
+            int relatoriosQtdPaginas;
+            String relatoriosEditora;
+            String relatoriosCidade;
+            
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Entre com a quantidade de paginas: ");
+            relatoriosQtdPaginas = scan.nextInt();
+            
+            scan = new Scanner(System.in);
+            System.out.print("Entre com a editora: ");
+            relatoriosEditora = scan.nextLine();
+            
+            System.out.print("Entre com a cidade: ");
+            relatoriosCidade = scan.nextLine();
+            
+            acervoRelatorios.setRelatoriosQtdPaginas(relatoriosQtdPaginas);
+            acervoRelatorios.setRelatoriosEditora(relatoriosEditora);
+            acervoRelatorios.setRelatoriosCidade(relatoriosCidade);
+            
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
