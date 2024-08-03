a6007f10d17a5e06304497d8d586095ad174b9ea
Caio Cezar Dias <caiocd007@gmail.com>
2024-08-03 14:36:56 -0300

    Views

---
+++ src/main/java/uemg/controllers/MySqlConnectionSingleton.java
@@ -0,0 +1,42 @@
+package uemg.controllers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySqlConnectionSingleton {
+    private Connection conn;
+    private static MySqlConnectionSingleton instance;
+
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost";
+    private static final String PORT = "3306";
+    private static final String USER = "root";
+    private static final String PASS = "LitTle1@";
+    private static final String DATABASE = "acervodb";
+    private static final String TIMEZONE = "useTimezone=true&serverTimezone=UTC";
+
+    private MySqlConnectionSingleton() {
+        String connect = URL + ":" + PORT + "/" + DATABASE + "?" + TIMEZONE;
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(connect, USER, PASS);
+            System.out.println("Conexão com o banco de dados estabeelecida com sucesso");
+        } catch (Exception ex) {
+            System.out.println("Erro ao conectar ao banco de dados");
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public static MySqlConnectionSingleton getInstance() {
+        if(instance == null) {
+            instance = new MySqlConnectionSingleton();
+        } else if(instance.getConn() == null) {
+            instance = new MySqlConnectionSingleton();
+        }
+        return instance;
+    }
+}
